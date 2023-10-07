c2d76aa26e04295e0185f41156c865b901c08040
Artem Furman <artemfurman@gmail.com>
2023-10-07 03:20:50 +0200

    env fix

--- flaw_to_crypto_converter/settings.py
+++ flaw_to_crypto_converter/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -99,16 +99,16 @@ WSGI_APPLICATION = 'flaw_to_crypto_converter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv("DB_ENGINE"),
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER", ""),
-        'PASSWORD': os.getenv("DB_PASSWORD", ""),
-        'HOST': os.getenv("DB_HOST", ""),
-        'PORT': os.getenv("DB_PORT", ""),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv("DB_ENGINE"),
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USER", ""),
+#         'PASSWORD': os.getenv("DB_PASSWORD", ""),
+#         'HOST': os.getenv("DB_HOST", ""),
+#         'PORT': os.getenv("DB_PORT", ""),
+#     }
+# }
 
 
 # Password validation
@@ -148,9 +148,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    FRONTEND_DIR / "build/static",
-]
+# STATICFILES_DIRS = [
+#     FRONTEND_DIR / "build/static",
+# ]
 
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -160,7 +160,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 """
 LOGGING = {
     'version': 1,
@@ -181,9 +180,9 @@ LOGGING = {
     },
 }
 """
-CORS_ORIGIN_ALLOW_ALL = os.getenv("DJANGO_CORS_ORIGIN_ALLOW_ALL") == "True"
+# CORS_ORIGIN_ALLOW_ALL = os.getenv("DJANGO_CORS_ORIGIN_ALLOW_ALL") == "True"
 
-CORS_ORIGIN_WHITELIST = os.getenv("DJANGO_CORS_ORIGIN_WHITELIST").split(",")
+# CORS_ORIGIN_WHITELIST = os.getenv("DJANGO_CORS_ORIGIN_WHITELIST").split(",")
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
